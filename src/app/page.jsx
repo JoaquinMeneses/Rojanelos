16a3e340510eaf0bbdfbb5346f38b1a6adad3137
Joaquin Meneses <joakinmeneses.jm@gmail.com>
2024-03-17 21:42:11 -0300

    add feature:guild info

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,37 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { apiUrl } from "@/utils/apiUrl";
 
 const App = () => {
+  const [totalRojanelos, setTotalRojanelos] = useState(0);
+  const [totalWatchlist, setTotalWatchlist] = useState(0);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${apiUrl}/rojanelos/watchlist`);
+        setTotalRojanelos(response.data.totalRojanelos);
+        setTotalWatchlist(response.data.totalWatchlist);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="grid place-content-center">
-      <h1>ROJANELOS</h1>
       <p>
         Protectores de Felpu y amantes del buen mate. $RON HOLDERS. Pixels
         Lovers.
       </p>
+      <div>
+        <p>Rojanelos en la Guild: {totalRojanelos}</p>
+        <p>Rojanelos en la Watchlist: {totalWatchlist}</p>
+      </div>
+      <p>Tamos laburando esta seccion aun :D</p>
     </div>
   );
 };
